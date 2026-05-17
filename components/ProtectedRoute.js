c4cea643aefb6e310298439eea7c840bc14c22ef
Sadiqunnabi17 <sadiqunnabi@gmail.com>
2026-05-18 00:02:06 +0600

    Frontend basic set up done

---
+++ components/ProtectedRoute.js
@@ -0,0 +1,21 @@
+"use client";
+import { useAuth } from "@/context/AuthContext";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import Spinner from "./Spinner";
+
+export default function ProtectedRoute({ children }) {
+  const { user, loading } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!loading && !user) {
+      router.push("/login");
+    }
+  }, [user, loading, router]);
+
+  if (loading) return <Spinner />;
+  if (!user) return null;
+
+  return children;
+}
