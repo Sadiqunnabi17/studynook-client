1f2ee846d9f31bf6dcf0999d25b747cd2aa5edb0
Sadiqunnabi17 <sadiqunnabi@gmail.com>
2026-05-21 21:22:06 +0600

    Frontend: fix axios baseURL to use env variable for dev and production

--- api/axios.js
+++ api/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "http://localhost:5000/api",
-  baseURL: "https://studynook-server-yxr0.onrender.com/api", // ← no localhost:5000, uses Next.js proxy
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api",
   withCredentials: true,
 });
 
